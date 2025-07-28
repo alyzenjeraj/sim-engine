a658ee9c551cdfd6484201bf2c17862d95833142
Alyzen Jeraj <alyzenjeraj@gmail.com>
2025-07-27 21:05:22 -0700

    wip implel sequence

--- src/btree/composite.rs
+++ src/btree/composite.rs
@@ -2,7 +2,14 @@ use super::node::BehaviourNode;
 use super::result::BTResult;
 use super::blackboard::Blackboard;
 
+// Runs the Children in Order
 pub struct Sequence {
     children: Vec<Box<dyn BehaviourNode>>,
     current: usize,
+}
+
+impl Sequence {
+    pub fn new(children: Vec<Box<dyn BehaviourNode>>) -> Self {
+        Sequence { children, current: 0 }
+    }
 }
